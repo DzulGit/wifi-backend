e68c7ffe8d82789952dffa340a6303b5df5a4cf5
TyoSaja <budisulistiyokds@.com>
2026-05-15 21:09:23 +0700

    feat: add automated invoice reminder system

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
 import { NotificationsController } from './notifications.controller'; 
 import { PrismaModule } from '../prisma/prisma.module'; 
+import { ReminderService } from './reminder.service';
 
 @Module({
   imports: [PrismaModule], 
   controllers: [NotificationsController],
-  providers: [NotificationsService],
+  providers: [NotificationsService, ReminderService],
   exports: [NotificationsService],
 })
 export class NotificationsModule {}
