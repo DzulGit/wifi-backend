44f07eec5ea57923cb7495649f6756f76630b46b
TyoSaja <budisulistiyokds@.com>
2026-05-18 08:52:11 +0700

    update: jika tidak langganan selama 3 bulan akan di suspend dan 3 bulan lagi akan di hapus

--- src/service-request/service-requests.module.ts
+++ src/service-request/service-requests.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ServiceRequestsService } from './service-requests.service';
 import { ServiceRequestsController } from './service-requests.controller';
+import { ServiceRequestsCron } from './service-requests.cron';
 import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule],
   controllers: [ServiceRequestsController],
-  providers: [ServiceRequestsService],
+  providers: [ServiceRequestsService, ServiceRequestsCron],
   exports: [ServiceRequestsService],
 })
 export class ServiceRequestsModule {}
